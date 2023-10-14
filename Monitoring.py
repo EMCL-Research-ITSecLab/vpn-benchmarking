d9a3909dac7b62fef0478a774866dab075cd24f9
lamr02n <manuel-f-02@proton.me>
2023-10-14 16:00:34 +0200

    Clean up files and add class for better output

--- Monitoring.py
+++ Monitoring.py
@@ -1,72 +1,109 @@
-import logging
+#!/usr/bin/env python3
+
+import curses
+import psutil
 import threading
 import time
 
-import psutil
+
+global interval
+interval = 0.5
 
 
+class Screen:
+		
+    def __init__(self) -> None:  
+        self.win = curses.initscr()
+    
+        curses.noecho()
+        curses.cbreak()
+        self.win.keypad(True)
+  
+    def __del__(self) -> None:
+        curses.nocbreak()
+        self.win.keypad(False)
+        curses.echo()
+        curses.endwin()
+    
+    
 class Monitoring:
-    format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
     done = threading.Event()
-    CUT_OFF = 1
-
+    
+    def __init__(self) -> None:
+        self.scr = Screen()
+        self.cpu_percent = self.HardwareValue(psutil.cpu_times_percent, 0)
+        self.ram_percent = self.HardwareValue(psutil.virtual_memory, 2)
+    
     def run(self, func):
-        logging.info(f"starting monitoring")
-
-        monitor = threading.Thread(target=self.__updating_values)
+        monitor = threading.Thread(target=self.poll)
         target = threading.Thread(target=func)
         monitor.start()
         target.start()
-
-        while target.is_alive():
-            time.sleep(0.5)
-
-        self.done.set()
-
-        logging.info("monitoring completed")
-
-    def __updating_values(self):
-        cpu_percent_sum = 0
-        ram_percent_sum = 0
-        pre_iterations = 1
-        iterations = 1
         
+        while target.is_alive():
+            time.sleep(interval)
+            
+        self.done.set()
+        
+    def poll(self):
         while not self.done.is_set():
-            cpu_percent = self.__get_cpu_percent()
-            ram_percent = self.__get_ram_percent()
-            print(f"CPU: {cpu_percent} %     RAM: {ram_percent} %", end="     \r")
+            # Hardware Performance
+            self.scr.win.addstr(0, 1, f"Performance:")
+            self.scr.win.addstr(2, 2, f"CPU:")
+            self.scr.win.addstr(2, 10, f"{self.cpu_percent.get()} %")
+            self.scr.win.addstr(3, 2, f"RAM:")
+            self.scr.win.addstr(3, 10, f"{self.ram_percent.get()} %")
             
-            if pre_iterations > self.CUT_OFF:
-                cpu_percent_sum += cpu_percent
-                ram_percent_sum += ram_percent
-                iterations += 1
-            else:
-                pre_iterations += 1
+            # Network Performance
+            self.scr.win.addstr(0, 30, f"Network:")
+            self.scr.win.addstr(2, 31, f"PPS:")
+            self.scr.win.addstr(2, 42, f"TODO packets per second")  # TODO: Add implementation
+            self.scr.win.addstr(3, 31, f"Upload:")
+            self.scr.win.addstr(3, 42, f"TODO Bytes")               # TODO: Add implementation
+            self.scr.win.addstr(4, 31, f"Download:")
+            self.scr.win.addstr(4, 42, f"TODO Bytes")               # TODO: Add implementation
+            
+            self.scr.win.refresh()
+            time.sleep(interval)
+        
+    class HardwareValue:
+        cut_off = 1     # number of cycles to ignore for average calculation
+        
+        def __init__(self, func, attr) -> None:
+            self.function = func
+            self.attribute = attr
+            self.sum = 0
+            self.iterations = 1 - self.cut_off
+            
+        def get(self):
+            value = self.function()[self.attribute]
+            
+            if self.iterations >= 1:
+                self.sum += value
                 
-            time.sleep(0.5)
+            self.iterations += 1
             
-        cpu_percent_avrg = self.__average(cpu_percent_sum, iterations - self.CUT_OFF)   # TODO: Add check for negative values
-        ram_percent_avrg = self.__average(ram_percent_sum, iterations - self.CUT_OFF)
-        print(f"  CPU average: {cpu_percent_avrg} %\n  RAM average: {ram_percent_avrg} %")
-            
-    def __average(self, sum, iterations):
-        return "{:4.1f}".format(sum / iterations)
-
-    def __get_cpu_percent(self):
-        return psutil.cpu_times_percent()[0]
-
-    def __get_ram_percent(self):
-        return psutil.virtual_memory()[2]
-
-
-# only for testing purposes
-def do_sth():
+            return value
+        
+        def get_average(self):
+            if self.iterations < 1:
+                return "{:4.1f}".format(0)
+            else:
+                return "{:4.1f}".format(self.sum / self.iterations)
+    
+    #class NetworkValue:
+        
+        # TODO: Add implementation
+                
+        
+# only for testing purposes     
+def test():
     i = 0
-    for _ in range(99999999):
+    for _ in range(499999999):
         i += 1
-
-
+        
+    
 if __name__ == "__main__":
     monitor = Monitoring()
-    monitor.run(do_sth)
+    monitor.run(test)
+    
