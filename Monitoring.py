5b1bb31d0838388e2c567b3c66998d825278f71a
lamr02n <manuel.f.02@web.de>
2023-10-30 12:47:16 +0100

    add pps count

--- Monitoring.py
+++ Monitoring.py
@@ -34,6 +34,8 @@ class Monitoring:
         self.ram_percent = self.HardwareValue(psutil.virtual_memory, 2)
         self.initial_bytes_sent = psutil.net_io_counters().bytes_sent
         self.initial_bytes_recv = psutil.net_io_counters().bytes_recv
+        self.pps_sent = 0
+        self.pps_recv = 0
     
     def run(self, func):
         monitor = threading.Thread(target=self.__poll)
@@ -49,6 +51,11 @@ class Monitoring:
     def __poll(self):
         while not self.done.is_set():
             printscr = lambda a, b, str : self.scr.win.addstr(a, b, str)
+            pps_sent = threading.Thread(target=self.__update_packets_per_second_sent)
+            pps_recv = threading.Thread(target=self.__update_packets_per_second_recv)
+            
+            pps_sent.start()
+            pps_recv.start()
             
             # Hardware Performance
             printscr(0, 1, f"Performance:")
@@ -59,12 +66,14 @@ class Monitoring:
             
             # Network Performance
             printscr(0, 30, f"Network:")
-            printscr(2, 31, f"PPS:")
-            printscr(2, 42, f"TODO packets per second")         # TODO: Add implementation
-            printscr(3, 31, f"Upload:")
-            printscr(3, 42, f"{self.__format_bytes(self.__get_upload_bytes())}     ")
-            printscr(4, 31, f"Download:")
-            printscr(4, 42, f"{self.__format_bytes(self.__get_download_bytes())}     ")
+            printscr(2, 31, f"PPS (sent):")
+            printscr(2, 50, f"{self.pps_sent} packets per second     ")
+            printscr(3, 31, f"PPS (received):")
+            printscr(3, 50, f"{self.pps_recv} packets per second     ")
+            printscr(4, 31, f"Upload:")
+            printscr(4, 50, f"{self.__format_bytes(self.__get_upload_bytes())}     ")
+            printscr(5, 31, f"Download:")
+            printscr(5, 50, f"{self.__format_bytes(self.__get_download_bytes())}     ")
             
             self.scr.win.refresh()
             time.sleep(interval)
@@ -81,13 +90,18 @@ class Monitoring:
     def __get_download_bytes(self):
         return psutil.net_io_counters().bytes_recv - self.initial_bytes_recv
         
-    def __get_packets_per_second():
-        # TODO: Implement
-        # nfstream
-        # ndpid
-        # socket
-        # Prozesse betrachten
-        pass
+    def __update_packets_per_second_sent(self):
+        while not self.done.is_set():
+            before = psutil.net_io_counters().packets_sent
+            time.sleep(interval)
+            self.pps_sent = (psutil.net_io_counters().packets_sent - before) / interval
+            
+    def __update_packets_per_second_recv(self):
+        while not self.done.is_set():
+            before = psutil.net_io_counters().packets_recv
+            time.sleep(interval)
+            self.pps_recv = (psutil.net_io_counters().packets_recv - before) / interval
+        
     
     class HardwareValue:
         cut_off = 1     # number of cycles to ignore for average calculation
