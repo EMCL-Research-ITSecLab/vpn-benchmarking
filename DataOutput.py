37066ce65a0d69cfbf509ca54f7e73466ded9307
lamr02n <manuel.f.02@web.de>
2023-11-21 17:27:32 +0100

    add rest of the values to DataOutput

--- DataOutput.py
+++ DataOutput.py
@@ -11,16 +11,20 @@ class DataOutput:
     lists = {
         "cpu_percent": [],
         "ram_percent": [],
-        "pps_sent": [],
-        "bytes_sent": []
+        "bytes_recv": [],
+        "bytes_sent": [],
+        "pps_recv": [],
+        "pps_sent": []
     }
     
     def make_graphs_for_directory(self, 
         dir_path,
         cpu_percent=False,
         ram_percent=False,
-        pps_sent=False,
-        bytes_sent=False
+        bytes_recv=False,
+        bytes_sent=False,
+        pps_recv=False,
+        pps_sent=False
     ):
         try:
             for file in os.listdir(dir_path):
@@ -30,8 +34,10 @@ class DataOutput:
                         file_path,
                         cpu_percent=cpu_percent,
                         ram_percent=ram_percent,
-                        pps_sent=pps_sent,
-                        bytes_sent=bytes_sent
+                        bytes_recv=bytes_recv,
+                        bytes_sent=bytes_sent,
+                        pps_recv=pps_recv,
+                        pps_sent=pps_sent
                     )
                     plt.close()
         except NotADirectoryError:
@@ -41,8 +47,10 @@ class DataOutput:
         file_path,
         cpu_percent=False,
         ram_percent=False,
-        pps_sent=False,
-        bytes_sent=False
+        bytes_recv=False,
+        bytes_sent=False,
+        pps_recv=False,
+        pps_sent=False
     ):
         self.file = open(file_path)
         self.data = json.load(self.file)
@@ -56,24 +64,30 @@ class DataOutput:
             file_name=file_name,
             cpu_percent=cpu_percent,
             ram_percent=ram_percent,
-            pps_sent=pps_sent,
-            bytes_sent=bytes_sent
+            bytes_recv=bytes_recv,
+            bytes_sent=bytes_sent,
+            pps_recv=pps_recv,
+            pps_sent=pps_sent
         )
     
     def __make_graph(self,
         file_name,
         cpu_percent,
         ram_percent,
-        pps_sent,
-        bytes_sent
+        bytes_recv,
+        bytes_sent,
+        pps_recv,
+        pps_sent
     ):
         self.__reset_lists()
         
         for i in range(len(self.data["data"])):
             if cpu_percent == True: self.lists["cpu_percent"].append(self.__get_cpu_percent(i))
             if ram_percent == True: self.lists["ram_percent"].append(self.__get_ram_percent(i))
-            if pps_sent == True: self.lists["pps_sent"].append(self.__get_pps_sent(i))
+            if bytes_recv == True: self.lists["bytes_recv"].append(self.__get_bytes_recv(i))
             if bytes_sent == True: self.lists["bytes_sent"].append(self.__get_bytes_sent(i))
+            if pps_recv == True: self.lists["pps_recv"].append(self.__get_pps_recv(i))
+            if pps_sent == True: self.lists["pps_sent"].append(self.__get_pps_sent(i))
         
         # TODO: Add way to plot multiple graphs into one graphic
         no_data = True
@@ -105,12 +119,17 @@ class DataOutput:
     def __get_ram_percent(self, entry):
         return self.data["data"][entry]["hardware"][0]["ram_percent"]
     
-    def __get_pps_sent(self, entry):
-        return self.data["data"][entry]["network"][0]["pps_sent"]
+    def __get_bytes_recv(self, entry):
+        return self.data["data"][entry]["network"][0]["bytes_recv"]
     
     def __get_bytes_sent(self, entry):
         return self.data["data"][entry]["network"][0]["bytes_sent"]
     
+    def __get_pps_recv(self, entry):
+        return self.data["data"][entry]["network"][0]["pps_recv"]
+    
+    def __get_pps_sent(self, entry):
+        return self.data["data"][entry]["network"][0]["pps_sent"]
 
 if __name__ == "__main__":
     args = sys.argv[1:]
