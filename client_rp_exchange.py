836d6f8d4e37bc3aa2a115bf16886767f0ceb289
lamr02n <manuel-f-02@proton.me>
2023-12-03 14:56:45 +0100

    format client_rp_exchange

--- client_rp_exchange.py
+++ client_rp_exchange.py
@@ -3,11 +3,11 @@ from HTTPExchange import HTTPExchange
 
 
 if __name__ == "__main__":
-	monitor = Monitoring("client_rp_exchange")
-	client = HTTPExchange.OnClient()
+    monitor = Monitoring("client_rp_exchange")
+    client = HTTPExchange.OnClient()
 
-	monitor.start(auto=False)
-	### start test
-	client.run_with_rp(monitor=monitor)
+    monitor.start(auto=False)
+    ### start test
+    client.run_with_rp(monitor=monitor)
     ### end test
-	monitor.stop()
+    monitor.stop()
