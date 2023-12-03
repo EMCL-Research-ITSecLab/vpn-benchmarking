c50305322036976f03c6fa489061155ed1518bbf
lamr02n <manuel-f-02@proton.me>
2023-12-03 15:00:12 +0100

    delete unused test file

--- test.py
+++
@@ -1,5 +0,0 @@
-from prompt_toolkit import prompt
-from prompt_toolkit.completion import PathCompleter
-
-file_path = prompt("Enter file path: ", completer=PathCompleter())
-print("You entered:", file_path)
