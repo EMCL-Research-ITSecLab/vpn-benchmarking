c4facb15bac1217b39a7422d48b5ee9526b47f3d
lamr02n <manuel-f-02@proton.me>
2023-12-03 14:54:49 +0100

    format client_genkeys

--- client_genkeys.py
+++ client_genkeys.py
@@ -1,7 +1,6 @@
-from Monitoring import Monitoring
 from HTTPExchange import HTTPExchange
 
 
-if __name__ == "__main__":        
+if __name__ == "__main__":
     client = HTTPExchange.OnClient()
     client.gen_keys(100)
