612b3471c4b0a00cd3793159ba409dea45e22ecd
lamr02n <manuel-f-02@proton.me>
2023-12-03 14:56:23 +0100

    format client_novpn_exchange

--- client_novpn_exchange.py
+++ client_novpn_exchange.py
@@ -3,11 +3,11 @@ from HTTPExchange import HTTPExchange
 
 
 if __name__ == "__main__":
-	monitor = Monitoring("client_novpn_exchange")
-	client = HTTPExchange.OnClient()
+    monitor = Monitoring("client_novpn_exchange")
+    client = HTTPExchange.OnClient()
 
-	monitor.start(auto=False)
-	### start test
-	client.run(100, monitor=monitor)
+    monitor.start(auto=False)
+    ### start test
+    client.run(100, monitor=monitor)
     ### end test
-	monitor.stop()
+    monitor.stop()
