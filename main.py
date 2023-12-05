3c3e1c3b952d55ccabe6e6b878be7ac47d15e0e1
lamr02n <manuel-f-02@proton.me>
2023-12-05 13:49:40 +0100

    format main

--- main.py
+++ main.py
@@ -106,12 +106,13 @@ def cli(role, iterations, operation):
 
 
 def install_requirements():
-    print("Installing pip requirements... ", end='', flush=True)
+    print("Installing pip requirements... ", end="", flush=True)
     try:
         subprocess.check_output(["pip", "install", "-r", "requirements.txt"])
         print("done.")
     except:
         print_err("Could not install requirements.")
+
 
 if __name__ == "__main__":
     install_requirements()
