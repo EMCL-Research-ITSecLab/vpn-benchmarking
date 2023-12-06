be65c24bffb84438cd543c99714af676f65d9ee6
lamr02n <manuel.f.02@web.de>
2023-12-06 12:02:08 +0100

    delete duplicate files

--- client_genkeys.py
+++
@@ -1,6 +0,0 @@
-from HTTPExchange import HTTPExchange
-
-
-if __name__ == "__main__":
-    client = HTTPExchange.OnClient()
-    client.gen_keys(100)
--- server_genkeys.py
+++
@@ -1,7 +0,0 @@
-from Monitoring import Monitoring
-from HTTPExchange import HTTPExchange
-
-
-if __name__ == "__main__":
-    server = HTTPExchange.OnServer()
-    server.gen_keys(100)
