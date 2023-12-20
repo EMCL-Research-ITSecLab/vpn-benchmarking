4d656e2b7d8ffdc3410e1afdd7b0d02b6c6b6325
lamr02n <manuel.f.02@web.de>
2023-12-20 12:41:04 +0100

    update requirements

--- install_requirements.py
+++ install_requirements.py
@@ -8,7 +8,7 @@ from pathlib import Path
 def install_pip_requirements():
     print("Installing pip requirements... ", end="", flush=True)
     try:
-        subprocess.check_output(["pip", "install", "-r", "requirements.txt"])
+        subprocess.check_output(["usr/pip", "install", "-r", "requirements.txt"])
         print("done.")
     except:
         print_err("Could not install requirements.")
