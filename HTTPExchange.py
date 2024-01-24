396879bccd287a2fbf8cdabc96397a4fcacc2027
lamr02n <manuel.f.02@web.de>
2024-01-24 16:44:51 +0100

    add count keys function

--- HTTPExchange.py
+++ HTTPExchange.py
@@ -185,8 +185,7 @@ class HTTPExchange:
                 )
 
         def __count_rp_keys(self):
-            exchange = HTTPExchange()
-            return exchange.count_rp_keys()
+            pass
 
         class Server(BaseHTTPRequestHandler):
             def do_GET(self):
@@ -374,8 +373,7 @@ class HTTPExchange:
                 )
 
         def __count_rp_keys(self):
-            exchange = HTTPExchange()
-            return exchange.count_rp_keys()
+            pass
 
         def __http_get(self, url, iface=None):
             c = pycurl.Curl()
@@ -388,50 +386,7 @@ class HTTPExchange:
             c.close()
 
     def count_rp_keys(self):
-        path = os.path.join(os.getcwd(), "rp-keys/")
-        s_pub_path = os.path.join(path, "server-public")
-        cl_pub_path = os.path.join(path, "client-public")
-
-        s_pub_count, cl_pub_count = 0, 0
-        try:
-            for _ in os.listdir(s_pub_path):
-                s_pub_count += 1
-        except:
-            print_err(
-                "The folder rp-keys/server-public could not be found/opened. Generate and share new keys to proceed."
-            )
-            return -1
-        try:
-            for _ in os.listdir(cl_pub_path):
-                cl_pub_count += 1
-        except:
-            print_err(
-                "The folder rp-keys/client-public could not be found/opened. Generate and share new keys to proceed."
-            )
-            return -1
-
-        # something went wrong if the number of keys in directories are unequal
-        if s_pub_count != cl_pub_count:
-            return -1
-
-        # check if this is server or client by checking existence of secret keys
-        s_key_path = os.path.join(path, "server-secret")
-        c_key_path = os.path.join(path, "client-secret")
-
-        count = 0
-        if os.path.exists(s_key_path):
-            for _ in os.listdir(s_key_path):
-                count += 1
-        elif os.path.exists(c_key_path):
-            for _ in os.listdir(c_key_path):
-                count += 1
-        else:
-            print_err("Key path was not found.")
-
-        if s_pub_count != count:
-            return -1
-
-        return s_pub_count
+        pass
 
     def send_file_to_host(self, file, target_user, target_ip_addr, target_path):
         try:
--- helpers.py
+++ helpers.py
@@ -5,13 +5,14 @@ import subprocess
 
 class Rosenpass:
     def __init__(self, role) -> None:
+        self.home_path = os.getcwd()
+
         if role not in ("server", "client"):
             messages.print_err("Unknown role as input.")
             self.role = None
             return
 
         self.role = role
-        self.home_path = os.getcwd()
 
     def generate_keys(self, iterations):
         if self.role == None:
@@ -95,6 +96,53 @@ class Rosenpass:
 
         messages.print_log(f"Generated keys for the {self.role}.")
 
+    def count_keys(self):
+        path = os.path.join(self.home_path, "rp-keys")
+        s_pub_path = os.path.join(path, "server-public")
+        cl_pub_path = os.path.join(path, "client-public")
+
+        s_pub_count, cl_pub_count = 0, 0
+
+        try:
+            for _ in os.listdir(s_pub_path):
+                s_pub_count += 1
+        except:
+            messages.print_err(
+                "The folder rp-keys/server-public could not be found/opened. Generate and share new keys to proceed."
+            )
+            return -1
+        try:
+            for _ in os.listdir(cl_pub_path):
+                cl_pub_count += 1
+        except:
+            messages.print_err(
+                "The folder rp-keys/client-public could not be found/opened. Generate and share new keys to proceed."
+            )
+            return -1
+
+        # something went wrong if the number of keys in directories are unequal
+        if s_pub_count != cl_pub_count:
+            return -1
+
+        # check if this is server or client by checking existence of secret keys
+        s_key_path = os.path.join(path, "server-secret")
+        c_key_path = os.path.join(path, "client-secret")
+
+        count = 0
+        if os.path.exists(s_key_path):
+            for _ in os.listdir(s_key_path):
+                count += 1
+        elif os.path.exists(c_key_path):
+            for _ in os.listdir(c_key_path):
+                count += 1
+        else:
+            messages.print_err("Key path was not found.")
+
+        if s_pub_count != count:
+            return -1
+
+        return s_pub_count
+
     def __create_key_directories(self):
         if self.role not in ("server", "client"):
             messages.print_err("Unable to create key directories: false or no role.")
@@ -117,9 +165,6 @@ class Rosenpass:
             return -1
 
         return 0
-
-    def count_keys(self):
-        pass
 
 
 def send_file_to_host():
