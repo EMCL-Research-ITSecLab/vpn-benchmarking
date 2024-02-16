03f4668c785c3cdde59da1c4dda2616fa2b07031
lamr02n <manuel-f-02@proton.me>
2024-02-16 15:41:28 +0100

    update main.py (still untested)

--- main.py
+++ main.py
@@ -1,134 +1,156 @@
-from Monitoring import Monitoring
-from old_version.HTTPExchange import HTTPExchange
-from messages import print_err
+from new_version.Server import *
+from new_version.Client import *
+from new_version.vpns import NoVPN, Rosenpass
+from new_version.exchanges import HTTP
+import messages
 
 import click
 
 
-def client_genkeys(number):
-    client = HTTPExchange.OnClient()
-    client.gen_keys(number)
+class HandleInput:
+    valid_inputs = False
+
+    def __init__(
+        self, role, vpn_option, exchange_type, operation, iterations, dir
+    ) -> None:
+        self.role = role
+        self.vpn_option = vpn_option
+        self.exchange_type = exchange_type
+        self.operation = operation
+        self.iterations = iterations
+        self.directory = dir
+
+        if self.__check_values():
+            self.valid_inputs = True
+
+    def execute(self) -> bool:
+        if not self.valid_inputs:
+            messages.print_err("Inputs are not valid. Please start again.")
+            return False
+
+        if not self.__create_instance():
+            return False
+
+        if self.operation == "keygen":
+            if not self.__handle_keygen():
+                return False
+        elif self.operation == "keysend":
+            if not self.__handle_keysend():
+                return False
+        elif self.operation == "exchange":
+            if not self.__handle_exchange():
+                return False
+        else:  # can not happen as long as values were checked and not changed
+            return False
+        return True
+
+    def __check_values(self) -> bool:
+        if self.role not in ("server", "client"):
+            messages.print_err("Invalid ROLE argument. Has to be server|client.")
+            return False
+
+        if self.vpn_option not in ("novpn", "rosenpass"):
+            messages.print_err(
+                "Invalid VPN_OPTION argument. Has to be novpn|rosenpass."
+            )
+            return False
+
+        if self.exchange_type not in ("http"):
+            messages.print_err("Invalid EXCHANGE_TYPE argument. Has to be http.")
+            return False
+
+        if self.operation not in ("keygen", "keysend", "exchange"):
+            messages.print_err(
+                "Invalid OPERATION argument. Has to be keygen|keysend|exchange."
+            )
+            return False
+
+        if self.iterations < 1:
+            messages.print_err("Invalid ITERATIONS option. Has to be positive.")
+            return False
+
+        return True
+
+    def __create_instance(self) -> bool:
+        if self.role == "server":
+            match self.exchange_type:
+                case "http":
+                    match self.vpn_option:
+                        case "novpn":
+                            self.instance = Server(HTTP, NoVPN)
+                            return True
+                        case "rosenpass":
+                            self.instance = Server(HTTP, Rosenpass)
+                            return True
+                        case _:
+                            return False
+                case _:
+                    return False
+        elif self.role == "client":
+            match self.exchange_type:
+                case "http":
+                    match self.vpn_option:
+                        case "novpn":
+                            self.instance = Client(HTTP, NoVPN)
+                            return True
+                        case "rosenpass":
+                            self.instance = Client(HTTP, Rosenpass)
+                            return True
+                        case _:
+                            return False
+                case _:
+                    return False
+        else:
+            return False
+
+    def __handle_keygen(self):
+        self.instance.keygen()
+
+    def __handle_keysend(self):
+        self.instance.keysend(self.directory)
+
+    def __handle_exchange(self):
+        self.instance.run(self.iterations)
 
 
-def client_share_keys(remote_dir):
-    client = HTTPExchange().OnClient()
-    client.send_public_keys_to_server(remote_dir)
+# EXCHANGE:
+# python main.py
+#   -i|--iterations 100
+#   server|client
+#   novpn|rosenpass
+#   http
+#   exchange
 
+# KEY GENERATION:
+# python main.py
+#   server|client
+#   novpn|rosenpass
+#   http
+#   keygen
 
-def client_novpn_exchange(iterations, auto=False):
-    monitor = Monitoring("client", "novpn")
-    client = HTTPExchange.OnClient()
-
-    monitor.start(auto=auto)
-    ### start test
-    client.run(iterations, monitor=monitor)
-    ### end test
-    monitor.stop()
-
-
-def client_rp_exchange(iterations=None, auto=False):
-    monitor = Monitoring("client", "rp")
-    client = HTTPExchange.OnClient()
-
-    monitor.start(auto=auto)
-    ### start test
-    client.run_with_rp(iterations=iterations, monitor=monitor)
-    ### end test
-    monitor.stop()
-
-
-def server_genkeys(number):
-    server = HTTPExchange.OnServer()
-    server.gen_keys(number)
-
-
-def server_share_keys(remote_dir):
-    server = HTTPExchange().OnServer()
-    server.send_public_keys_to_client(remote_dir)
-
-
-def server_novpn_exchange(iterations, auto=False):
-    monitor = Monitoring("server", "novpn")
-    server = HTTPExchange.OnServer()
-
-    monitor.start(auto=auto)
-    ### start test
-    server.run(iterations, monitor=monitor)
-    ### end test
-    monitor.stop()
-
-
-def server_rp_exchange(iterations=None, auto=False):
-    monitor = Monitoring("server", "rp")
-    server = HTTPExchange.OnServer()
-
-    monitor.start(auto=auto)
-    ### start test
-    server.run_with_rp(iterations=iterations, monitor=monitor)
-    ### end test
-    monitor.stop()
-
-
-# EXAMPLES:
-# python main.py --role server -i 100                  # execute exchange for server with 100 iterations
-# python main.py genkeys --role server -i 100          # generate 100 keys for server
-# python main.py genkeys -r server --iterations 100    # generate 100 keys for server
-# python main.py genkeys -r server -i 50               # generate 50 keys for server
+# SENDING PUBLIC KEYS:
+# python main.py
+#   server|client
+#   novpn|rosenpass
+#   http
+#   keysend
 
 
 @click.command()
 @click.option("-i", "--iterations", type=int, default=1, help="number of iterations")
-@click.option("--auto", help="monitor in auto mode", is_flag=True)
 @click.option(
     "-d", "--dir", type=str, help="directory to save the keys (only for keysend option)"
 )
 @click.argument("role", type=str)
+@click.argument("vpn_option", type=str)
+@click.argument("exchange_type", type=str)
 @click.argument("operation", type=str)
-def cli(role, iterations, operation, dir, auto):
-    if role == None:
-        print_err("Something went wrong.")
-    elif role == "server":
-        if operation == "keygen":
-            server_genkeys(iterations)
-        elif operation == "keysend":
-            if dir != None:
-                try:
-                    server_share_keys(dir)
-                except:
-                    print_err("Directory could not be reached.")
-            else:
-                print_err(
-                    "Missing directory for the keys on the remote host (use option -d)."
-                )
-        elif operation == "novpn":
-            server_novpn_exchange(iterations, auto)
-        elif operation == "rp":
-            server_rp_exchange(iterations, auto)
-        else:
-            print("OPERATION must be keygen/keysend/novpn/rp.")
-    elif role == "client":
-        if operation == "keygen":
-            client_genkeys(iterations)
-        elif operation == "keysend":
-            if dir != None:
-                try:
-                    client_share_keys(dir)
-                except:
-                    print_err("Directory could not be reached.")
-            else:
-                print_err(
-                    "Missing directory for the keys on the remote host (use option -d)."
-                )
-        elif operation == "novpn":
-            client_novpn_exchange(iterations, auto)
-        elif operation == "rp":
-            client_rp_exchange(iterations, auto)
-        else:
-            print("OPERATION must be keygen/keysend/novpn/rp.")
-    else:
-        print_err("ROLE must be server|client.")
-        return
+def cli(role, vpn_option, exchange_type, operation, iterations, dir):
+    return (role, vpn_option, exchange_type, operation, iterations, dir)
 
 
 if __name__ == "__main__":
-    cli()
+    inputs = cli()
+    handler = HandleInput(
+        inputs[0], inputs[1], inputs[2], inputs[3], inputs[4], inputs[5]
+    )
---
+++ main2.py
@@ -0,0 +1,129 @@
+from Monitoring import Monitoring
+from old_version.HTTPExchange import HTTPExchange
+from messages import print_err
+
+import click
+
+
+def client_rosenpass_keygen(number):
+    client = HTTPExchange.OnClient()
+    client.gen_keys(number)
+
+
+def client_share_keys(remote_dir):
+    client = HTTPExchange().OnClient()
+    client.send_public_keys_to_server(remote_dir)
+
+
+def client_novpn_exchange(iterations, auto=False):
+    monitor = Monitoring("client", "novpn")
+    client = HTTPExchange.OnClient()
+
+    monitor.start(auto=auto)
+    ### start test
+    client.run(iterations, monitor=monitor)
+    ### end test
+    monitor.stop()
+
+
+def client_rp_exchange(iterations=None, auto=False):
+    monitor = Monitoring("client", "rp")
+    client = HTTPExchange.OnClient()
+
+    monitor.start(auto=auto)
+    ### start test
+    client.run_with_rp(iterations=iterations, monitor=monitor)
+    ### end test
+    monitor.stop()
+
+
+def server_genkeys(number):
+    server = HTTPExchange.OnServer()
+    server.gen_keys(number)
+
+
+def server_share_keys(remote_dir):
+    server = HTTPExchange().OnServer()
+    server.send_public_keys_to_client(remote_dir)
+
+
+def server_novpn_exchange(iterations, auto=False):
+    monitor = Monitoring("server", "novpn")
+    server = HTTPExchange.OnServer()
+
+    monitor.start(auto=auto)
+    ### start test
+    server.run(iterations, monitor=monitor)
+    ### end test
+    monitor.stop()
+
+
+def server_rp_exchange(iterations=None, auto=False):
+    monitor = Monitoring("server", "rp")
+    server = HTTPExchange.OnServer()
+
+    monitor.start(auto=auto)
+    ### start test
+    server.run_with_rp(iterations=iterations, monitor=monitor)
+    ### end test
+    monitor.stop()
+
+
+
+
+@click.command()
+@click.option("-i", "--iterations", type=int, default=1, help="number of iterations")
+@click.option("--auto", help="monitor in auto mode", is_flag=True)
+@click.option(
+    "-d", "--dir", type=str, help="directory to save the keys (only for keysend option)"
+)
+@click.argument("role", type=str)
+@click.argument("operation", type=str)
+def cli(role, iterations, operation, dir, auto):
+    if role == None:
+        print_err("Something went wrong.")
+    elif role == "server":
+        if operation == "keygen":
+            server_genkeys(iterations)
+        elif operation == "keysend":
+            if dir != None:
+                try:
+                    server_share_keys(dir)
+                except:
+                    print_err("Directory could not be reached.")
+            else:
+                print_err(
+                    "Missing directory for the keys on the remote host (use option -d)."
+                )
+        elif operation == "novpn":
+            server_novpn_exchange(iterations, auto)
+        elif operation == "rp":
+            server_rp_exchange(iterations, auto)
+        else:
+            print("OPERATION must be keygen/keysend/novpn/rp.")
+    elif role == "client":
+        if operation == "keygen":
+            client_rosenpass_keygen(iterations)
+        elif operation == "keysend":
+            if dir != None:
+                try:
+                    client_share_keys(dir)
+                except:
+                    print_err("Directory could not be reached.")
+            else:
+                print_err(
+                    "Missing directory for the keys on the remote host (use option -d)."
+                )
+        elif operation == "novpn":
+            client_novpn_exchange(iterations, auto)
+        elif operation == "rp":
+            client_rp_exchange(iterations, auto)
+        else:
+            print("OPERATION must be keygen/keysend/novpn/rp.")
+    else:
+        print_err("ROLE must be server|client.")
+        return
+
+
+if __name__ == "__main__":
+    cli()
