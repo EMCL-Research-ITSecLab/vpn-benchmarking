0b7db83d1ab236401d55666963473049f558fef7
lamr02n <94921205+lamr02n@users.noreply.github.com>
2024-03-08 15:14:26 +0100

    documentation for NoVPN.py

--- src/vpns/NoVPN.py
+++ src/vpns/NoVPN.py
@@ -2,21 +2,40 @@ from src.vpns.VPN import VPN
 
 
 class NoVPN(VPN):
+    """
+    Implements the baseline without VPN. No VPN is used, therefore most methods just return a success.
+    """
+
     def __init__(self, role) -> None:
+        """
+        :param role: role of the host
+        """
         super().__init__(role)
 
     def open(self) -> bool:
-        # does not open any vpn
+        """
+        Does not open a VPN.
+        :return: returns True, since does not do anything
+        """
         return True
 
     def close(self) -> bool:
-        # does not close any vpn
+        """
+        Does not close any VPN.
+        :return: returns True, since does not do anything
+        """
         return True
 
     def generate_keys(self):
-        # no keys needed
+        """
+        Does not need to generate keys, uses no keys.
+        :return: returns True, since does not do anything
+        """
         return True
 
     def share_pubkeys(self, remote_path) -> bool:
-        # no keys needed
+        """
+        Does not send any keys, uses no keys.
+        :return: returns True, since does not do anything
+        """
         return True
