3907de9ef538365de3ee93cbf5d305c01a5af51e
lamr02n <94921205+lamr02n@users.noreply.github.com>
2024-03-08 14:44:56 +0100

    documentation for Exchange.py

--- src/exchanges/Exchange.py
+++ src/exchanges/Exchange.py
@@ -2,7 +2,18 @@ import src.messages as messages
 
 
 class Exchange:
+    """
+    Base class for exchanges. To add a new exchange type, a class must inherit from Exchange.
+    """
+
     def __init__(self, role, open_server_address, open_server_port=9999, interface=None) -> None:
+        """
+        Loads all values needed for an exchange. Checks if the role is valid, throws an Exception otherwise.
+        :param role: role of the host, server or client
+        :param open_server_address: address on which the server will be reachable
+        :param open_server_port: publicly reachable port to be opened, default: 9999
+        :param interface: name of the interface of the VPN used during the exchange or None
+        """
         self.open_server_address = open_server_address
         self.open_server_port = open_server_port
         self.interface = interface
@@ -12,9 +23,13 @@ class Exchange:
                 "Unable to initialize exchange: unknown role. Known: server, client"
             )
             raise Exception("Unknown role")
-        else:
-            self.role = role
+
+        self.role = role
 
     def run(self) -> bool:
+        """
+        Executes the exchange with the set parameters. Implementation is done in inheriting classes.
+        :return: True for success, False otherwise
+        """
         messages.print_err("Exchange.run(self): NOT IMPLEMENTED")
         return False
