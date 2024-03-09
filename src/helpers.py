311b5a384077f1e5bea19956a126149ede0b929a
lamr02n <94921205+lamr02n@users.noreply.github.com>
2024-03-09 13:43:26 +0100

    documentation for helpers.py

--- src/helpers.py
+++ src/helpers.py
@@ -2,18 +2,31 @@ import subprocess
 
 import src.messages as messages
 
+"""
+Any functions that are universal in a way that they can be used by multiple classes can be put here.
+"""
+
 
 def send_file_to_host(file, target_user, target_ip_addr, target_path) -> bool:
+    """
+    Sends the given file to a remote host with the given username and IP address into the given remote directory.
+    :param file: file to be sent
+    :param target_user: username on the remote host
+    :param target_ip_addr: IP address of the remote host
+    :param target_path: directory for putting the file in on the remote host
+    :return: True for success, False otherwise
+    """
     try:
         subprocess.check_output(
             ["scp", "-pr", file, f"{target_user}@{target_ip_addr}:{target_path}"],
             stderr=subprocess.PIPE,
         )
-    except Exception as err:
+        return True
+    except subprocess.CalledProcessError:
         messages.print_err(
-            "SSH connection could not be established. Check if the SSH keys are correct, see the following error:"
+            "SSH connection could not be established. Check if the SSH keys are correct."
         )
+        return False
+    except Exception as err:
         print(f"{err=}")
         return False
-
-    return True
