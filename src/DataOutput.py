e0f17b4064dae3ebf8bfea8e26c96f1097af3d95
lamr02n <94921205+lamr02n@users.noreply.github.com>
2024-03-11 13:02:07 +0100

    documentation for DataOutput.py

--- src/DataOutput.py
+++ src/DataOutput.py
@@ -5,15 +5,36 @@ from src.messages import *
 
 
 class DataOutput:
+    """
+    Base class for different types of data outputs. An implementation can use the data from a file and generate some
+    kind of visualization from it, like graphs, tables or charts.
+    """
+
     def generate(self):
+        """
+        Generates the visual representation. Throws a NotImplementedError in base implementation.
+        """
         print_err("DataOutput.generate(self): NOT IMPLEMENTED")
         raise NotImplementedError
 
 
 class GraphOutput(DataOutput):
+    """
+    Implements the graph output.
+    """
+
     def __init__(self, path: str, value_types: list, detailed_full: [bool, bool], normal_median: [bool, bool]):
+        """
+        Checks if at least one of the detailed, full and normal, median pairs of booleans is True, otherwise raises
+        ValueError. Checks if path exists, otherwise throws FileNotFoundError. Sets the parameters.
+        :param path: path of a directory if JSON files with correct format or a single JSON file
+        :param value_types: list of value_types to generate graphs for
+        :param detailed_full: a pair of booleans, first stands for generate detailed graphs, second for full graphs
+        :param normal_median: a pair of booleans, first stands for generate normal graphs, second for min-max-median
+        graphs
+        """
         if detailed_full == [False, False] or normal_median == [False, False]:
-            return
+            raise ValueError
 
         if not os.path.exists(path):
             raise FileNotFoundError
@@ -24,6 +45,10 @@ class GraphOutput(DataOutput):
         self.normal_median = normal_median
 
     def generate(self):
+        """
+        Implements the generation for graphs. Creates a MultiFileGraphHandler with the initial parameters, goes through
+        all combinations of input pairs and generates respective graphs.
+        """
         generator = MultiFileGraphHandler(self.path, self.value_types)
 
         if self.detailed_full[0] and self.normal_median[0]:
