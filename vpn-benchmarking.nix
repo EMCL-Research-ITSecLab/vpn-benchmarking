25f3f3d4dfc7253b7606b2e892ddf593429f9582
SeZven <95926192+SeZven@users.noreply.github.com>
2024-07-31 13:12:09 +0200

    Update vpn-benchmarking.nix

--- vpn-benchmarking.nix
+++ vpn-benchmarking.nix
@@ -1,24 +1,31 @@
 # vpn-benchmarking.nix
-{
-  stdenv,
-  fetchFromGitHub,
-  pkgs ? import <nixpkgs> {},
+{ fetchFromGitHub
+, pkgs ? import <nixpkgs> { }
+,
 }:
 
-
-
-stdenv.mkDerivation {
-  pname = "mysoftware";
-  version = "1.0.0";  src = fetchFromGitHub {
+pkgs.stdenv.mkDerivation {
+  name = "vpnbenchmarking";
+  version = "1.0.0";
+  src = fetchFromGitHub {
     owner = "EMCL-Research-ITSecLab";
     repo = "vpn-benchmarking";
-    rev = "bf27feb";
-    sha256 = "sha256-zZGqUVDyyAW1VEZGz/OOh5qzhPGPv26iogmN9mZnvZ8=";
+    rev = "773145e";
+    sha256 = "...";
   };
 
+  propagatedBuildInputs = [
+    pkgs.python3
+    pkgs.virtualenv
+  ];
 
   installPhase = ''
-   mkdir -p $out
-   cp -r $src/* $out/
+    mkdir -p $out
+    cp -r  $src/src/ $out/
+    cp -r  $src/main.py $out/
+    cp -r  $src/set_hosts.py $out/
+    cp -r  $src/requirements.txt $out/
+
+    ${pkgs.python3.interpreter} -m venv "$out/.venv"
   '';
 }
